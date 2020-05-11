e95ba83d67035ce2f0f16747dc58c77428d55f99
Luke Gonsalves <gonluke14@hotmail.co.uk>
2020-05-11 14:05:26 +0100

    Main menu (simple)

---
+++ Assets/SelectScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectScene : MonoBehaviour {
+
+    public void changeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+
+        if (sceneName == "quit")
+        {
+            Application.Quit();
+            Debug.Log("game quit");
+        }
+    }
+
+    public void thisScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+}
